a0a1c963ce98c5c428aa0fc3cd76a9d4c41d74ca
darcrand <darcrandex@gmail.com>
2023-09-05 12:07:23 +0800

    cors

--- server/src/app/(api)/notes/route.ts
+++ server/src/app/(api)/notes/route.ts
@@ -1,7 +1,12 @@
-import { NextResponse } from "next/server";
-
 export async function GET() {
   const data = [{ id: "aa", title: "note aa" }];
 
-  return NextResponse.json({ data });
+  return new Response(JSON.stringify(data), {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "orzpass.netlify.app",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  });
 }
