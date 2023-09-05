5aed77164503f8036e9cdf4e46f5243a2486d889
darcrand <darcrandex@gmail.com>
2023-09-05 09:16:59 +0800

    clear

--- src/stores/use-user.ts
+++
@@ -1,9 +0,0 @@
-import { User } from '@/types/user'
-import { atom, useAtom } from 'jotai'
-
-const stateAtom = atom<User | undefined>(undefined)
-
-export function useUser() {
-  const [user, setUser] = useAtom(stateAtom)
-  return { user, setUser }
-}
