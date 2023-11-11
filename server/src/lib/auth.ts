3c6ce1fd0a8fc13ef6ad3d8e87ca2f1a768857d6
darcrand <darcrandex@gmail.com>
2023-11-11 15:10:03 +0800

    🦄 refactor(api): 替换 jwt 方案

--- server/src/lib/auth.ts
+++ server/src/lib/auth.ts
@@ -3,25 +3,28 @@
  * @description jsonwebtoken 和 jose 在 nextjs 中都存在问题
  */
 
-import CryptoJS from 'crypto-js'
+import jsonwebtoken from 'jsonwebtoken'
 
 const DEFAULT_EXP = 60 * 60 * 1000
 const secret = process.env.NEXT_APP_JWT_SECRET || 'secret'
 
 function sign(payload: any, exp = DEFAULT_EXP) {
-  const content = JSON.stringify({
-    payload,
-    exp: Date.now() + Math.max(exp, 0),
-  })
+  // const content = JSON.stringify({
+  //   payload,
+  //   exp: Date.now() + Math.max(exp, 0),
+  // })
 
-  return CryptoJS.AES.encrypt(content, secret).toString()
+  // return CryptoJS.AES.encrypt(content, secret).toString()
+  return jsonwebtoken.sign(payload, secret)
 }
 
 function verify(jwt: string) {
   try {
-    const content = CryptoJS.AES.decrypt(jwt, secret).toString(CryptoJS.enc.Utf8)
-    const { exp } = JSON.parse(content)
-    return exp > Date.now()
+    // const content = CryptoJS.AES.decrypt(jwt, secret).toString(CryptoJS.enc.Utf8)
+    // const { exp } = JSON.parse(content)
+    // return exp > Date.now()
+    jsonwebtoken.verify(jwt, secret)
+    return true
   } catch (error) {
     console.error('jwt verify error', error)
     return false
@@ -29,9 +32,13 @@ function verify(jwt: string) {
 }
 
 function decode<T = any>(jwt: string) {
-  const content = CryptoJS.AES.decrypt(jwt, secret).toString(CryptoJS.enc.Utf8)
-  const { payload } = JSON.parse(content)
-  return payload as T
+  // const content = CryptoJS.AES.decrypt(jwt, secret).toString(CryptoJS.enc.Utf8)
+  // const { payload } = JSON.parse(content)
+  // return payload as T
+
+  const content = jsonwebtoken.decode(jwt)
+
+  return content as T
 }
 
 export const jwt = { sign, verify, decode }
